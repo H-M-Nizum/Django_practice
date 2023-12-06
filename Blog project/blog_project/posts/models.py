ecce5da88b5970996c4107b0da8627bbab00f599
H-M-Nizum <hmnizum1714032@gmail.com>
2023-12-06 14:21:17 +0600

    project setup and creating model relationships

---
+++ Blog project/blog_project/posts/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from categories.models import Category
+from author.models import Author
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    
+    # built many to many relationship (post and category)
+    # akta post ar modde multiple category thakte pare abar aki category r multiple post hote parre.
+    category = models.ManyToManyField(Category)
+    
+    # many to one relationship
+    # akjon author multiple post likhe ba muultiple post ar akjon author thakte pare
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
