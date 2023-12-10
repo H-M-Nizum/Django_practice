47a72f4cf34a19c0fa7ca11054d48e2f5f5cc813
H-M-Nizum <hmnizum1714032@gmail.com>
2023-12-11 02:08:00 +0600

    18-Blog project part-2 :- 18-2 Login Page Part 1

--- 18-Blog project-part2/blog_project/author/views.py
+++ 18-Blog project-part2/blog_project/author/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from . import forms
-
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 # Create your views here.
 
 # def add_author(request):
@@ -19,8 +21,30 @@ def register(request):
         register_form = forms.RegistrationForm(request.POST)
         if register_form.is_valid():
             register_form.save()
+            messages.success(request, 'Account created successfully')
             return redirect('register')
         
     else:
         register_form = forms.RegistrationForm()
-    return render(request, 'register.html', {'form': register_form})
+    return render(request, 'register.html', {'form': register_form, 'type': 'register'})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            user_name = form.cleaned_data['username']
+            user_pass = form.cleaned_data['password']
+            user = authenticate(username = user_name, passsword = user_pass)
+            if user is not None:
+                messages.success(request, 'Account login successfully')
+                login(request, user)
+                return redirect('profile')
+            else:
+                messages.warning(request, 'Login info incorrect')
+                return redirect('register')
+            
+    else:
+        form = AuthenticationForm()
+        return render(request, 'register.html', {'form': form, 'type': 'Login'})
+            
