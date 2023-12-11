e6e987fbb68132e74c51ea1f59ea830fa13dac88
H-M-Nizum <hmnizum1714032@gmail.com>
2023-12-11 16:15:39 +0600

    18-Blog project part2:- 18-8 Prepopulated Field using slug

--- 18-Blog project-part2/blog_project/categories/models.py
+++ 18-Blog project-part2/blog_project/categories/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True, null=True, blank=True)
     
     
     # database a name show korar jonne
