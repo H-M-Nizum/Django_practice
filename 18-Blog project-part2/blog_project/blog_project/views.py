662bc4dd69eac743a1363a7d3e9a8429b8906439
H-M-Nizum <hmnizum1714032@gmail.com>
2023-12-11 16:53:40 +0600

    18-Blog project part2 :- 18-9 Filtering Posts By Category

--- 18-Blog project-part2/blog_project/blog_project/views.py
+++ 18-Blog project-part2/blog_project/blog_project/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from posts.models import Post
+from categories.models import Category
 
 # Create your views here.
 
-def home(request):
+def home(request, category_slug = None):
     data = Post.objects.all()
-    print(data)
-    return render(request, 'home.html', {'data': data})
+    if category_slug is not None:
+        category = Category.objects.get(slug = category_slug)
+        data = Post.objects.filter(category = category)
+    
+    categories = Category.objects.all()
+    
+    return render(request, 'home.html', {'data': data, 'category': categories})
